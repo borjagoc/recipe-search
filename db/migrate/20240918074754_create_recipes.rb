e9dd58f8efc972e1dff1b8e9a56baac420620d82
Borja Gonzalez <borja.gonzalez@generationhome.com>
2024-09-18 09:05:14 +0100

    Create recipes and add json data

---
+++ db/migrate/20240918074754_create_recipes.rb
@@ -0,0 +1,16 @@
+class CreateRecipes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.integer :cook_time
+      t.integer :prep_time
+      t.string :ingredients, array: true, default: []
+      t.decimal :ratings, precision: 3, scale: 2
+      t.string :cuisine
+      t.string :category
+      t.string :author
+      t.string :image
+      t.timestamps
+    end
+  end
+end
