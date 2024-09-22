dd4eaae0ab785fcbb1050a9ca7e39a31258e6c07
Borja Gonzalez <borja.gonzalez@generationhome.com>
2024-09-22 12:48:25 +0100

    Create user model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_18_075533) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_22_112528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "recipes", force: :cascade do |t|
+  create_table "recipes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "title"
     t.integer "cook_time"
     t.integer "prep_time"
@@ -24,6 +24,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_18_075533) do
     t.string "category"
     t.string "author"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
